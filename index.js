fad95d4288c7ab380a8f29657bfe4eaa47e1c101
Kaan <kaanasutay10@gmail.com>
2024-03-03 18:22:55 +0300

    move router

--- index.js
+++ index.js
@@ -50,18 +50,25 @@ app.all('/', (req, res) => {
     })
 })
 
-// auth:
-app.use('/auth', require('./src/routes/auth'))
-// user:
-app.use('/users', require('./src/routes/user'))
-// flight:
-app.use('/flights', require('./src/routes/flight'))
-// passenger:
-app.use('/passengers', require('./src/routes/passenger'))
-// reservation:
-app.use('/reservations', require('./src/routes/reservation'))
-// document:
-app.use('/documents', require('./src/routes/document'))
+//? Move to routes/index.js:
+// // auth:
+// app.use('/auth', require('./src/routes/auth'))
+// // user:
+// app.use('/users', require('./src/routes/user'))
+// // flight:
+// app.use('/flights', require('./src/routes/flight'))
+// // passenger:
+// app.use('/passengers', require('./src/routes/passenger'))
+// // reservation:
+// app.use('/reservations', require('./src/routes/reservation'))
+// // document:
+// app.use('/documents', require('./src/routes/document'))
+
+// Routes:
+// app.use('/', require('./src/routes/index.js'))
+// app.use('/', require('./src/routes/index'))
+// app.use('/', require('./src/routes/'))
+app.use(require('./src/routes/'))
 
 /* ------------------------------------------------------- */
 
