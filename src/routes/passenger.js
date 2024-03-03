1b41b28a180a1eb9c620cc1ecae76efaa1094cb3
Kaan <kaanasutay10@gmail.com>
2024-03-03 18:07:43 +0300

    passengerRoutes ok

--- src/routes/passenger.js
+++ src/routes/passenger.js
@@ -2,3 +2,23 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/passenger:
+
+const passenger = require('../controllers/passenger')
+
+// URL: /passengers
+
+router.route('/')
+    .get(passenger.list)
+    .post(passenger.create)
+
+router.route('/:id')
+    .get(passenger.read)
+    .put(passenger.update)
+    .patch(passenger.update)
+    .delete(passenger.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
