12b4fa61495c26889255329a801e16691aa8537c
Kaan <kaanasutay10@gmail.com>
2024-03-03 15:52:56 +0300

    restart userModel

--- src/models/user.js
+++ src/models/user.js
@@ -15,51 +15,7 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 // User Model:
 
-const passwordEncrypt = require('../helpers/passwordEncrypt')
 
-const UserSchema = new mongoose.Schema({
-
-    username: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-    },
-
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        set: (password) => passwordEncrypt(password)
-    },
-
-    email: {
-        type: String,
-        trim: true,
-        required: [true, 'Email field must be required'],
-        unique: [true, 'There is this email. Email field must be unique'],
-        validate: [
-            (email) => email.includes('@') && email.includes('.'),
-            'Email type is not correct.'
-        ]
-    },
-
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-
-    isStaff: {
-        type: Boolean,
-        default: false,
-    },
-
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-
-}, { collection: 'users', timestamps: true })
 
 /* ------------------------------------------------------- */
 module.exports = mongoose.model('User', UserSchema)
