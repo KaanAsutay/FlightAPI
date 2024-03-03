1f5e8ca736b40fb314f7928c80f3a1dc78a06524
Kaan <kaanasutay10@gmail.com>
2024-03-03 18:14:16 +0300

    reservationRoutes ok

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -2,3 +2,25 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/reservation:
+
+const reservation = require('../controllers/reservation')
+
+// URL: /reservations
+
+router.use(permissions.isStaffOrAdmin)
+
+router.route('/')
+    .get(reservation.list)
+    .post(reservation.create)
+
+router.route('/:id')
+    .get(reservation.read)
+    .put(reservation.update)
+    .patch(reservation.update)
+    .delete(permissions.isAdmin, reservation.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
