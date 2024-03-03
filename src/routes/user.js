9382ba2a85e31eaec1c64dc70f068220fb4d8f1c
Kaan <kaanasutay10@gmail.com>
2024-03-03 16:57:15 +0300

    userRoutes ok

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,14 +12,14 @@ const user = require('../controllers/user')
 // URL: /users
 
 router.route('/')
-    .get(permissions.isAdmin, user.list)
+    .get(user.list)
     .post(user.create)
 
 router.route('/:id')
-    .get(permissions.isLogin, user.read)
-    .put(permissions.isLogin, user.update)
-    .patch(permissions.isLogin, user.update)
-    .delete(permissions.isAdmin, user.delete)
+    .get(user.read)
+    .put(user.update)
+    .patch(user.update)
+    .delete(user.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
