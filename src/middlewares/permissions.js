b229bdf8f7fe66d46583a314789e2abb89536aee
Kaan <kaanasutay10@gmail.com>
2024-03-04 15:27:20 +0300

    permissions restart

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -6,39 +6,6 @@
 
 module.exports = {
 
-    isLogin: (req, res, next) => {
+    
 
-        return next()
-        
-        if (req.user) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
-
-    isAdmin: (req, res, next) => {
-
-        return next()
-        
-        if (req.user && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
-    },
-
-    isStaff: (req, res, next) => {
-
-        return next()
-        
-        if (req.user && (req.user.isAdmin || req.user.isStaff)) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Staff.')
-        }
-    },
 }
