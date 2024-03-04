cfe6a378928ef55698eae4f8a22c10f3ae880291
Kaan <kaanasutay10@gmail.com>
2024-03-04 15:31:36 +0300

    isAdmin ok

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -30,6 +30,13 @@ module.exports = {
 
     isAdmin: (req, res, next) => {
 
+        if (req.user && req.user.isAdmin) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Admin.')
+        }
+
     },
 
 }
