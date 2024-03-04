cd68a17ade67895db260ec7e4e3f069f064171e9
Kaan <kaanasutay10@gmail.com>
2024-03-04 15:29:24 +0300

    isLogin ok

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -6,6 +6,23 @@
 
 module.exports = {
 
-    
+    isLogin: (req, res, next) => {
+
+        if (req.user) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login.')
+        }
+
+    },
+
+    isStaff: (req, res, next) => {
+
+    },
+
+    isAdmin: (req, res, next) => {
+
+    },
 
 }
