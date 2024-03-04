0dfb1ff9b19eea5413cb9084da9c3c30a8ac7e48
Kaan <kaanasutay10@gmail.com>
2024-03-04 23:27:13 +0300

    # production | development NODE_ENV=development

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -8,6 +8,8 @@ module.exports = {
 
     isLogin: (req, res, next) => {
 
+       if (process.env.NODE_ENV == 'development') return next()
+
         if (req.user) {
             next()
         } else {
@@ -19,6 +21,9 @@ module.exports = {
 
     isStaffOrAdmin: (req, res, next) => {
 
+        if (process.env.NODE_ENV == 'development') return next()
+
+
         if (req.user && (req.user.isStaff || req.user.isAdmin)) {
             next()
         } else {
@@ -29,6 +34,9 @@ module.exports = {
     },
 
     isAdmin: (req, res, next) => {
+
+        if (process.env.NODE_ENV == 'development') return next()
+
 
         if (req.user && req.user.isAdmin) {
             next()
