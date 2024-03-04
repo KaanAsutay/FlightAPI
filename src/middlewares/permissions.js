f0b240986d2faa91d4a8eefbcb4144a72ab809d1
Kaan <kaanasutay10@gmail.com>
2024-03-04 15:32:52 +0300

    isStaff change isStaffOrAdmin

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -17,9 +17,9 @@ module.exports = {
 
     },
 
-    isStaff: (req, res, next) => {
+    isStaffOrAdmin: (req, res, next) => {
 
-        if (req.user && req.user.isStaff) {
+        if (req.user && (req.user.isStaff || req.user.isAdmin)) {
             next()
         } else {
             res.errorStatusCode = 403
