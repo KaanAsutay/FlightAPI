cb6eaf4666333e2abb0e2ad178cfb9cb0d4fbfa7
Kaan <kaanasutay10@gmail.com>
2024-03-04 15:30:52 +0300

    isStaff ok

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -19,6 +19,13 @@ module.exports = {
 
     isStaff: (req, res, next) => {
 
+        if (req.user && req.user.isStaff) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Staff.')
+        }
+
     },
 
     isAdmin: (req, res, next) => {
