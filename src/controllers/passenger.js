ad0423c232434fd8f6a5ec72064c5ac5cd16c589
Kaan <kaanasutay10@gmail.com>
2024-03-05 19:20:32 +0300

    add schema

--- src/controllers/passenger.js
+++ src/controllers/passenger.js
@@ -38,11 +38,11 @@ module.exports = {
             #swagger.parameters['body'] = {
                 in: 'body',
                 required: true,
-                schema: {
-                   
-                }
+                schema: { $ref: '#/definitions/Passenger' }
             }
         */
+
+        // req.body.createdId = req.user._id
 
         const data = await Passenger.create(req.body)
 
@@ -74,9 +74,7 @@ module.exports = {
             #swagger.parameters['body'] = {
                 in: 'body',
                 required: true,
-                schema: {
-                    
-                }
+                schema: { $ref: '#/definitions/Passenger' }
             }
         */
 
